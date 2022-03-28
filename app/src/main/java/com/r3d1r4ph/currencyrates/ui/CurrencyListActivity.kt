dd1e1f648cf85271d6d76eaba17804e498bd667d
Armen Gevorkyan <rediraph@mail.ru>
2022-03-28 13:53:16 +0700

    layout for currency list

--- app/src/main/java/com/r3d1r4ph/currencyrates/ui/CurrencyListActivity.kt
+++ app/src/main/java/com/r3d1r4ph/currencyrates/ui/CurrencyListActivity.kt
@@ -1,11 +1,12 @@
-package com.r3d1r4ph.currencyrates
+package com.r3d1r4ph.currencyrates.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.r3d1r4ph.currencyrates.R
 
-class MainActivity : AppCompatActivity() {
+class CurrencyListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_currency_list)
     }
 }
